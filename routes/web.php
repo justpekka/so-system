9dec82700b62983d1865a73f2e84b118b03ebe29
justpekka <ariefkhrisna631@gmail.com>
2022-02-02 16:20:41 +0700

    still first commit.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return 'Hello World!';
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
